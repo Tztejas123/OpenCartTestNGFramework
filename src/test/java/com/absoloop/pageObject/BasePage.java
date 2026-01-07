cd5b1b0db66aa7bb6d1038e77d38995ac4e8f52f
Tejas Zombade <tztejaszombade@gmail.com>
2026-01-07 11:24:59 +0530

    Add .gitignore and remove build artifacts

--- src/test/java/com/absoloop/pageObject/BasePage.java
+++ src/test/java/com/absoloop/pageObject/BasePage.java
@@ -6,11 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 public class BasePage {
-
 	WebDriver driver;
-
 	public BasePage(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
